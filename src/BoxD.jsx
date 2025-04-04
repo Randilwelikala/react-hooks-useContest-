056033e31acf6347f9f248cd2a5df40c76ad2cb2
randilwelikala <randilgimantha646@gmail.com>
2025-04-04 22:47:31 +0530

    add function to change background color

--- src/BoxD.jsx
+++ src/BoxD.jsx
@@ -4,8 +4,14 @@ import { NameContext } from "./BoxA";
 const BoxD = ({name})=>{
 
     const {name:newName,theme,toggleTheme} = useContext(NameContext);
+
+    const themeStyle = {
+        backgroundColor:theme ==='light'?'#fff':'#333',
+        color: theme === 'light'?'#000':'#fff',
+
+    }
     return(
-        <div className="box">
+        <div className="box" style={themeStyle}>
             <h1>Box D</h1>
             <p>name = {name}</p>
             <p>New Name = {newName}</p>
