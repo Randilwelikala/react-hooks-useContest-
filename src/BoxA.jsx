bc0cb72bf3a7b9a27f363abb1ad2d215b4995457
randilwelikala <randilgimantha646@gmail.com>
2025-04-04 22:14:35 +0530

    create setTheme useState

--- src/BoxA.jsx
+++ src/BoxA.jsx
@@ -5,6 +5,11 @@ export const NameContext = createContext();
 
 const BoxA  = ()=>{
     const[name,setName]= useState('React');
+    const[theme,setTheme] = useState('light');
+
+    const toggleTheme = ()=>{
+        
+    }
     return(
         <div className="box">
             <h1>Box A</h1>
